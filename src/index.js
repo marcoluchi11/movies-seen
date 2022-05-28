c92a8ecccdd651e0874d4efbe1627114dadbd5ae
marcoluchi11 <marcoaluchi@hotmail.com>
2022-05-28 18:12:16 -0300

    add provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import MoviesProvider from "./context/MoviesContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoviesProvider>
+      <App />
+    </MoviesProvider>
   </React.StrictMode>
 );
 
