59766a44e4a3758053991aa6149011df39a40af0
marcoluchi11 <marcoaluchi@hotmail.com>
2022-05-31 17:54:55 -0300

    add states passed to compnents for search

--- src/context/MoviesContext.jsx
+++ src/context/MoviesContext.jsx
@@ -1,9 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 export const MoviesContext = createContext();
 const MoviesProvider = ({ children }) => {
-  const [data, setData] = useState(0);
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const fetchData = async () => {
+    const url = `https://www.omdbapi.com/?s=${search}&apikey=89a340b7`;
+    console.log(url);
+    const res = await fetch(url);
+    const rta = await res.json();
+    setData(rta.Search);
+  };
+
   return (
-    <MoviesContext.Provider value={{ data }}>{children}</MoviesContext.Provider>
+    <MoviesContext.Provider value={{ data, setSearch, search, fetchData }}>
+      {children}
+    </MoviesContext.Provider>
   );
 };
 
