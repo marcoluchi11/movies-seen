2be935d529a2fa826c2fcbffe43739c1e8fa7300
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-01 17:07:10 -0300

    add design for movies on list

--- src/components/UserMoviesList.jsx
+++ src/components/UserMoviesList.jsx
@@ -1,14 +1,40 @@
-import { useContext } from "react";
+import styled from "@emotion/styled";
+import { nanoid } from "nanoid";
+import { Fragment, useContext } from "react";
 import { MoviesContext } from "../context/MoviesContext";
+import BackButton from "./BackButton";
 
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  div {
+    margin: 1rem;
+    padding: 1rem;
+  }
+`;
 const UserMoviesList = () => {
   const { list } = useContext(MoviesContext);
-  return list.map((item) => (
-    <div>
-      <h1>{item.Name}</h1>
-      <img src={item.Poster} alt="" />
-    </div>
-  ));
+  if (list.length === 0)
+    return (
+      <div>
+        <h1>Your list is empty</h1>
+        <BackButton />
+      </div>
+    );
+  return (
+    <Fragment>
+      <Container>
+        {list.map((item) => (
+          <div key={nanoid()}>
+            <h1>{item.Title}</h1>
+            <img src={item.Poster} alt="" />
+          </div>
+        ))}
+      </Container>
+      <BackButton />
+    </Fragment>
+  );
 };
 
 export default UserMoviesList;
