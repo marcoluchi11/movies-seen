1623944a27878af8fa32c52546b26c1393130d3f
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-01 10:57:04 -0300

    add state for error and listuser

--- src/context/MoviesContext.jsx
+++ src/context/MoviesContext.jsx
@@ -1,19 +1,30 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 export const MoviesContext = createContext();
 const MoviesProvider = ({ children }) => {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
-
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
   const fetchData = async () => {
     const url = `https://www.omdbapi.com/?s=${search}&apikey=89a340b7`;
-    console.log(url);
     const res = await fetch(url);
     const rta = await res.json();
     setData(rta.Search);
   };
 
   return (
-    <MoviesContext.Provider value={{ data, setSearch, search, fetchData }}>
+    <MoviesContext.Provider
+      value={{
+        data,
+        error,
+        setSearch,
+        search,
+        fetchData,
+        setError,
+        setList,
+        list,
+      }}
+    >
       {children}
     </MoviesContext.Provider>
   );
