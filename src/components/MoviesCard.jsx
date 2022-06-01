55e4821f9dec3432961e0426f86231c609d38ede
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-01 10:57:30 -0300

    add onclick for adding to list by user

--- src/components/MoviesCard.jsx
+++ src/components/MoviesCard.jsx
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { useContext } from "react";
+import { MoviesContext } from "../context/MoviesContext";
 
 const Container = styled.div`
   display: flex;
@@ -10,6 +12,7 @@ const Container = styled.div`
   transition: 0.5s background-color ease;
   transition: 0.5s color ease;
   border-radius: 5px;
+
   &&:hover {
     background-color: #000;
     color: #fff;
@@ -33,12 +36,27 @@ const Container = styled.div`
     background-color: #007aff;
     color: #fff;
   }
+  h2 {
+    position: absolute;
+    top: 5rem;
+    right: 2rem;
+    padding: 0.3rem 0.7rem;
+    background-color: lime;
+    border-radius: 25px;
+    font-size: 1.9rem;
+    cursor: pointer;
+  }
 `;
 const MoviesCard = ({ item }) => {
+  const { setList, list } = useContext(MoviesContext);
+  const handleClick = (item) => {
+    setList([...list, item]);
+  };
   return (
     <Container>
       <h1>{item.Title}</h1>
-      <img src={item.Poster} alt="poster" />
+      <img onClick={() => handleClick(item)} src={item.Poster} alt="poster" />
+      <h2>+</h2>
       <p>{item.Year}</p>
     </Container>
   );
