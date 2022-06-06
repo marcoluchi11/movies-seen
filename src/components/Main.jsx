4007ff675e5e0754a32b539f924b75ec22681590
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-06 15:16:55 -0300

    add error component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,28 @@
+import styled from "@emotion/styled";
+import { useContext } from "react";
+import { MoviesContext } from "../context/MoviesContext";
+import Error from "./Error";
 import MoviesFetch from "./MoviesFetch";
 import Search from "./Search";
 
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  p {
+    text-align: center;
+    align-items: center;
+  }
+`;
 const Main = () => {
+  const { error } = useContext(MoviesContext);
   return (
-    <section>
+    <Container>
       <Search />
+      {error && (
+        <Error message="You need to login to add movies to Your List" />
+      )}
       <MoviesFetch />
-    </section>
+    </Container>
   );
 };
 
