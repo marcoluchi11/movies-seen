5e1970d1bac3e8d8763812b836399d007dd6ec1f
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-06 15:17:49 -0300

    add delete button for cards on list

--- src/components/UserMoviesList.jsx
+++ src/components/UserMoviesList.jsx
@@ -8,13 +8,35 @@ const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+
   div {
+    position: relative;
     margin: 1rem;
     padding: 1rem;
+    .listimage {
+      width: 15rem;
+      height: 15rem;
+    }
+  }
+`;
+const Imagen = styled.img`
+  position: absolute;
+  top: 7rem;
+  right: 1rem;
+  background-color: #fff;
+  border-radius: 50%;
+
+  &&:hover {
+    cursor: pointer;
+    background-color: #000;
+    color: #fff;
   }
 `;
 const UserMoviesList = () => {
-  const { list } = useContext(MoviesContext);
+  const { list, setList } = useContext(MoviesContext);
+  const handleDelete = (elem) => {
+    setList(list.filter((item) => item.imdbID !== elem.imdbID));
+  };
   if (list.length === 0)
     return (
       <div>
@@ -28,7 +50,13 @@ const UserMoviesList = () => {
         {list.map((item) => (
           <div key={nanoid()}>
             <h1>{item.Title}</h1>
-            <img src={item.Poster} alt="" />
+            <img className="listimage" src={item.Poster} alt="" />
+            <Imagen
+              onClick={() => handleDelete(item)}
+              src="https://icongr.am/fontawesome/close.svg?size=32&color=000000
+            "
+              alt="close"
+            />
           </div>
         ))}
       </Container>
