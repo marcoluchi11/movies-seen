211e9319950007ef668a7dda6c961338d084fe0c
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-08 12:00:43 -0300

    add list when logged from db

--- src/components/Auth/IsAuthenticated.jsx
+++ src/components/Auth/IsAuthenticated.jsx
@@ -1,6 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import styled from "@emotion/styled";
+import Axios from "axios";
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { MoviesContext } from "../../context/MoviesContext";
 import Login from "./Login";
 import Logout from "./Logout";
 import Profile from "./Profile";
@@ -12,8 +15,20 @@ const Container = styled.div`
   align-items: center;
 `;
 const IsAuthenticated = () => {
-  const { isAuthenticated } = useAuth0();
+  const { setList } = useContext(MoviesContext);
+  const { isAuthenticated, user } = useAuth0();
 
+  useEffect(() => {
+    if (user) {
+      Axios.get("http://localhost:3001/read", {
+        params: {
+          user: user.email,
+        },
+      })
+        .then((response) => setList(response.data))
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
   return (
     <Container>
       {isAuthenticated ? (
