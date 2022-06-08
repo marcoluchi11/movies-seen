9154995eec22c730b7aa9eb7ece30320d7ff9718
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-08 19:15:36 -0300

    add j-content

--- src/components/MoviesFetch.jsx
+++ src/components/MoviesFetch.jsx
@@ -7,6 +7,7 @@ const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
+  justify-content: center;
 `;
 const MoviesFetch = () => {
   const { data } = useContext(MoviesContext);
