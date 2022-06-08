0e465c36048453138081c1dd459d7150dd587bf9
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-08 19:11:43 -0300

    delete heading and change in styles

--- src/components/Auth/Profile.jsx
+++ src/components/Auth/Profile.jsx
@@ -3,10 +3,15 @@ import styled from "@emotion/styled";
 const Container = styled.section`
   display: flex;
   flex-direction: column;
+  align-items: center;
+
   img {
-    width: 14rem;
-    height: 14rem;
-    border-radius: 15px;
+    width: 5rem;
+    height: 5rem;
+    border-radius: 100%;
+  }
+  h3 {
+    margin: 1rem;
   }
 `;
 const Profile = () => {
@@ -18,8 +23,7 @@ const Profile = () => {
     isAuthenticated && (
       <Container>
         <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>Email : {user.email}</p>
+        <h3>{user.name}</h3>
       </Container>
     )
   );
