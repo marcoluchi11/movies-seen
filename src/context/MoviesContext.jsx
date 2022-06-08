57fc5145edcd329b997afe71787f5dc0f03e8690
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-08 19:06:25 -0300

    add loading and error with more details

--- src/context/MoviesContext.jsx
+++ src/context/MoviesContext.jsx
@@ -3,18 +3,19 @@ export const MoviesContext = createContext();
 const MoviesProvider = ({ children }) => {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState({ state: false, message: "" });
   const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
   const fetchData = async () => {
     const url = `https://www.omdbapi.com/?s=${search}&apikey=89a340b7`;
     const res = await fetch(url);
     const rta = await res.json();
     if (rta.Response === "True") {
-      setError(false);
+      setError({ ...error, state: false });
       setData(rta.Search);
     } else {
       setData(rta.Error);
-      setError(true);
+      setError({ state: true, message: "Movie Not found, try again" });
     }
   };
 
@@ -24,6 +25,8 @@ const MoviesProvider = ({ children }) => {
         data,
         error,
         setSearch,
+        loading,
+        setLoading,
         search,
         fetchData,
         setError,
